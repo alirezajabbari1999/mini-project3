d8d8b245fae86dc4abf73e31b2b9a393443cd56a
Alireza <Alirezammww@gmail.com>
2023-11-02 22:26:14 +0330

    added files

---
+++ src/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Header.css"
+import Nav from './Nav'
+
+export default function Header() {
+    return(
+      <div className='header-container'>
+        <Nav />
+      </div>
+    )
+  }
---
+++ src/Header/Nav.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Nav.css";
+
+export default function Nav() {
+  return (
+    <div className="nav-background">
+      <div className="nav-container">
+        <div className="logo-box">
+          <img src="./../src/Images/logo.png" alt="" />
+          <span>KASUKA</span>
+        </div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
